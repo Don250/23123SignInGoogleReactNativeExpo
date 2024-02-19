27411d8a257fdcdb865ba97d4edb5ae9cfc40e2b
Don250 <shyakadon@gmail.com>
2024-02-19 17:57:44 +0200

    'first'

--- App.js
+++ App.js
@@ -1,10 +1,63 @@
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+// web clientId: 429830643239-2m7b288lbdglu78g7sb8666gmhghvs71.apps.googleusercontent.com
+// ios clientId: 429830643239-c71835bib5o5i8r1l7m6leqgune88u1l.apps.googleusercontent.com
+// android clientId:
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [accessToken, setAccessToken] = React.useState(null);
+  const [user, setUser] = React.useState(null);
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    iosClientId: "429830643239-c71835bib5o5i8r1l7m6leqgune88u1l.apps.googleusercontent.com",
+    clientId: "429830643239-2m7b288lbdglu78g7sb8666gmhghvs71.apps.googleusercontent.com"
+  });
+
+  React.useEffect(() => {
+    if (response?.type === "success") {
+      setAccessToken(response.authentication.accessToken);
+      accessToken && fetchUserInfo();
+    }
+  }, [response, accessToken]);
+
+  async function fetchUserInfo() {
+    let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    });
+    const userInfo = await response.json();
+    setUser(userInfo);
+  }
+
+  const ShowUserInfo = () => {
+    if (user) {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Image source={{ uri: user.picture }} style={{ width: 100, height: 100, borderRadius: 50 }} />
+          <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{user.name}</Text>
+        </View>
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {user && <ShowUserInfo />}
+      {user === null && (
+        <>
+          <Text style={{ fontSize: 35, fontWeight: 'bold' }}>Welcome to  Google</Text>
+          <Text style={{ fontSize: 25, fontWeight: 'bold', marginBottom: 20, color: 'gray' }}>Please SignIn </Text>
+          <TouchableOpacity disabled={!request} onPress={() => promptAsync()}>
+          <Image source={require("../loginReact/assets/GoogleLog.png")} style={{ width: 50 ,height: 50 , }} Sign in with Google />
+          <Text> Sign in with Google</Text>
+
+          </TouchableOpacity>
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
